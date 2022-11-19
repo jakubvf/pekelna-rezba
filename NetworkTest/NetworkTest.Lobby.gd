257fc362b5f93141d86cbc5f60881180cd51b048
Jakub V. Flasar <jakub@flasar.net>
2022-11-19 15:41:13 +0100

    Fix connection bugs and try create server after starting up

--- NetworkTest/NetworkTest.Lobby.gd
+++ NetworkTest/NetworkTest.Lobby.gd
@@ -2,28 +2,36 @@ extends Control
 
 var SERVER_IP = "127.0.0.1"
 var SERVER_PORT = 65443
-var MAX_PLAYERS = 10
+var MAX_PLAYERS = 8
 
 var player_info = {}
 var my_info = { name = "Johnson Magenta", favorite_color = Color8(255, 0, 255) }
 
-signal player_info_changed(player_info)
-
-func _ready():
-	get_tree().connect("network_peer_connected", self, "_player_connected")
-	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
-	get_tree().connect("connected_to_server", self, "_connected_ok")
-	get_tree().connect("connection_failed", self, "_connected_fail")
-	get_tree().connect("server_disconnected", self, "_server_disconnected")
+signal player_connected(new_player_info)
+signal player_disconnected(id)
 
 func _player_connected(id):
 	# Called on both clients and server when a peer connects. Send my info to it.
 	rpc_id(id, "register_player", my_info)
 	$Panel.visible = false
 
+remote func register_player(info):
+	var id = get_tree().get_rpc_sender_id()
+	print("player connected: " + str(id))
+	player_info[id] = info
+	emit_signal("player_connected", info)
+
+
 func _player_disconnected(id):
 	player_info.erase(id) # Erase player from info.
+	rpc_id(id, "deregister_player", id)
 	$Panel.visible = true
+
+remote func deregister_player(id):
+	print("player disconnected: " + str(id))
+	player_info.erase(id)
+	emit_signal("player_disconnected", id)
+
 
 func _connected_ok():
 	print("connected: "  + SERVER_IP + ":" + str(SERVER_PORT))
@@ -39,15 +47,22 @@ func _connected_fail():
 
 var player_scene = preload("res://NetworkTest/NetworkTest.Player.tscn")
 
-remote func register_player(info):
-	var id = get_tree().get_rpc_sender_id()
-	print("player connected: " + str(id))
-	player_info[id] = info
-	emit_signal("player_info_changed")
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connection_failed", self, "_connected_fail")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+	
+	var server_result = start_server()
+	if server_result == ERR_CANT_CREATE:
+		# TODO: handle start_client fail
+		start_client()
+	
+	$Panel.visible = false
 
 func _on_Server_pressed():
 	start_server()
-	$Panel.visible = false
 
 func start_client():
 	print("connecting to: " + SERVER_IP + ":" + str(SERVER_PORT))
@@ -62,5 +77,9 @@ func _on_Client_pressed():
 func start_server():
 	print("starting server: " + SERVER_IP + ":" + str(SERVER_PORT))
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_server(SERVER_PORT, MAX_PLAYERS)
-	get_tree().network_peer = peer
+	
+	var result = peer.create_server(SERVER_PORT, MAX_PLAYERS)
+	if result == OK:
+		get_tree().network_peer = peer
+		
+	return result
--- NetworkTest/NetworkTest.PlayerContainer.gd
+++ NetworkTest/NetworkTest.PlayerContainer.gd
@@ -11,9 +11,13 @@ func _ready():
 	NetLobby.connect("player_info_changed", self, "player_info_changed")
 
 var player_scene = load("res://NetworkTest/NetworkTest.Player.tscn")
-func player_info_changed():
-	for child in get_children():
-		remove_child(child)
+func player_info_changed(new_info):
+	var selfPeerID = get_tree().get_network_unique_id()
+	print(str(selfPeerID), "connected")	
 	
-	for player in NetLobby.player_info:
-		add_child(player_scene.instance())
+	var new_player = player_scene.instance()
+	new_player.set_name(str(selfPeerID))
+	new_player.set_network_master(selfPeerID)
+	add_child(new_player)
+
+# TODO: player disconnect
